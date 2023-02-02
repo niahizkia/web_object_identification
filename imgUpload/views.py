9698071aafd712040dff67bf3afaff1067cd538a
niahizkia <niahizkia255@gmail.com>
2023-02-02 21:23:02 +0700

    adding form for image upload

--- imgUpload/views.py
+++ imgUpload/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .forms import ImageUploadForm
+def handle_uploaded_file(f):
+    with open('img.jpg', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def imageprocess(request):
+    form = ImageUploadForm(request.POST, request.FILES)
+    if form.is_valid():
+        handle_uploaded_file(request.FILES['image'])
+    return render(request, 'result.html')
